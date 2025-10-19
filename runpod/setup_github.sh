07307fb6c276d3a1c17905ec42a9fb099c34cd9e
Christina Lu <christina.ty.lu@gmail.com>
2025-10-19 22:10:59 +0000

    Change github setup script to also set up chatspace/ repo.

--- runpod/setup_github.sh
+++ runpod/setup_github.sh
@@ -10,6 +10,7 @@ github_url=${3:-"git@github.com:lu-christina/persona-subspace.git"}
 # 0) Setup git
 git config --global user.email "$email"
 git config --global user.name "$name"
+git config --global pull.rebase true
 
 # 1) Setup SSH key
 ssh-keygen -t ed25519 -C "$email"
@@ -18,6 +19,9 @@ read -p "Have you added the SSH key to https://github.com/settings/keys? (y/Y/ye
 while [[ ! "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; do
     read -p "Please type 'y', 'Y', or 'yes' after adding the SSH key: " response
 done
+
+cd /root/git
+git clone git@github.com:GallagherCommaJack/chatspace
 
 # 2) Project specific setup (if github_url is provided)
 if [ -n "$github_url" ]; then
